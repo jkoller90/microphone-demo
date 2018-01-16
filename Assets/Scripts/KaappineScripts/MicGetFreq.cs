41daef6cef0e2a46d5f17e860eb10f961db7a6db
jkoller90 <jmk4411@gmail.com>
2018-01-16 12:36:42 -0500

    MicGetFreq added to new scene called kaappineFrequencyTutorial.needs testing though; Spawn By Loudness now works

--- Assets/Scripts/KaappineScripts/MicGetFreq.cs
+++ Assets/Scripts/KaappineScripts/MicGetFreq.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-public class MicrophoneInput_ : MonoBehaviour {
+public class MicGetFreq : MonoBehaviour {
 	private AudioSource audio_;	
 	public float sensitivity = 100;
 	public float loudness = 10; 
-	public int sampleCount = 12; 
-	 //low end of realtime is 12
+	public int sampleCount = 12;
+	public int sampleRate = 11024;
 	void Start() {
 		audio_ = GetComponent<AudioSource>();	
 		audio_.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
@@ -30,5 +30,21 @@ public class MicrophoneInput_ : MonoBehaviour {
 		foreach(float s in data)
 			a += Mathf.Abs(s);
 		return a/sampleCount; 
+	}
+
+	public float GetFrequency(){
+		float fundamentallFrequency = 0.0f;
+		float[] data = new float[8192];
+		audio_.GetSpectrumData (data, 0, FFTWindow.BlackmanHarris);
+		float s = 0.0f;
+		int i = 0;
+		for (int n = 0; n < 8192; n++) {
+			if (s < data [n]) {
+				s = data [n]; 
+				i = n;
+			}
+		}
+		fundamentallFrequency = i * sampleCount / 8192;
+		return fundamentallFrequency;
 	}
 }
